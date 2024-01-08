ff085146408880a675eaedddbba9649f3d58c09a
kill4us <kill4usmxy@gmail.com>
2024-01-08 22:16:16 +0800

    add client

--- game/src/client.py
+++ game/src/client.py
@@ -7,8 +7,10 @@ from thrift.transport import TSocket
 from thrift.transport import TTransport
 from thrift.protocol import TBinaryProtocol
 
+from sys import stdin
 
-def main():
+
+def operate(op, user_id, username, score):
     # Make socket
     transport = TSocket.TSocket('localhost', 9090)
 
@@ -24,12 +26,20 @@ def main():
     # Connect!
     transport.open()
     
-    user = User(1, 'kill4us', 15000)
+    user = User(user_id, username, score)
 
-    client.add_user(user,"")
+    if op == "add":
+        client.add_user(user,"")
+    elif op =="remove":
+        client.remove_user(user, "")
 
 
     transport.close()
+
+def main():
+    for line in stdin:
+        op, user_id, username, score = line.split(' ')
+        operate(op, int(user_id), username, int(score))
 
 if __name__== "__main__":
     main()
